b356aab43c786de321506fe6df9c44e88835e3c2
popipomie <Kaewkanlayamarasri@gmail.com>
2025-12-01 17:22:50 +0700

    Workshop #HTML - FORM 01

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>67160122 </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css./bootstrap.css">
+
+</head>
+
+<body>
+    <style>
+    body {
+        font-family: "Itim", cursive;
+        font-weight: 400;
+        font-style: normal;
+    }
+    </style>
+    <div class="container pt-5">
+        <h1> Workshop #HTML - FORM </h1>
+
+        <div class="row mt-3 align-items-center">
+            <div class="col-auto">
+                <label for="fname" class="col-form-label">ชื่อ</label>
+            </div>
+
+            <div class="col">
+                <input type="text" id="fname" class="form-control">
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+</body>
+
+</html>
