12d3768dc101ccbfd1db121879f32b32fc08c7f0
popipomie <Kaewkanlayamarasri@gmail.com>
2025-12-22 17:45:45 +0700

    lab flight

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@ Route::get('view2', function () {
 Route::get('/user', [App\Http\Controllers\user::class, 'index']);
 Route::post('/user', [App\Http\Controllers\user::class, 'store']);
 
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::get('/flights', 'FlightController@index');
+    Route::get('flight/{id}', 'FlightController@update');
+    Route::post('/flight', 'FlightController@store');
+});
+
 
